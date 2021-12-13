a757a49889e74b2d607a10d3754f50dcbbe7205c
OvchinnikovNV <bugaga.dada@mail.ru>
2021-12-13 21:50:33 +0300

    Updates

---
+++ precache-manifest.72c7506ca5497ab34f20822aad3afcd2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc7482c06b1a34f990f4120b4a51543",
+    "url": "/MyPythonProject/index.html"
+  },
+  {
+    "revision": "b955bd76501acaeca3ad",
+    "url": "/MyPythonProject/static/css/main.6a4e5fea.chunk.css"
+  },
+  {
+    "revision": "02273179fd0a4ce8ad6b",
+    "url": "/MyPythonProject/static/js/2.510b6f58.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/MyPythonProject/static/js/2.510b6f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b955bd76501acaeca3ad",
+    "url": "/MyPythonProject/static/js/main.38d4c84e.chunk.js"
+  },
+  {
+    "revision": "fc37c8662b2f5b697c91",
+    "url": "/MyPythonProject/static/js/runtime-main.98b4d71f.js"
+  },
+  {
+    "revision": "0634fc1378e12761480b7669682a0977",
+    "url": "/MyPythonProject/static/media/paper.0634fc13.png"
+  },
+  {
+    "revision": "d9ab5b5e85e12c34d76fc3bf351354b7",
+    "url": "/MyPythonProject/static/media/scissors.d9ab5b5e.png"
+  },
+  {
+    "revision": "82874705930a753c3f63c8ff3ec55199",
+    "url": "/MyPythonProject/static/media/stone.82874705.png"
+  },
+  {
+    "revision": "081232d0cbe4b2e3402e6e53634c36e3",
+    "url": "/MyPythonProject/static/media/vs.081232d0.png"
+  }
+]);
