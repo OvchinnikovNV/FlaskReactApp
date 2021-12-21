a8f0faff3e1ae88a96075e2f85ad24d50986d112
OvchinnikovNV <bugaga.dada@mail.ru>
2021-12-21 03:55:35 +0300

    Updates

---
+++ precache-manifest.5963031cd4acafc1088a7cbccd989d1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec15a16581b3cbff839003fdb7f71d97",
+    "url": "/MyPythonProject/index.html"
+  },
+  {
+    "revision": "ce9abaef34b20df012df",
+    "url": "/MyPythonProject/static/css/main.f9095b11.chunk.css"
+  },
+  {
+    "revision": "51b32d98f1fdc80abe9f",
+    "url": "/MyPythonProject/static/js/2.1a261898.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/MyPythonProject/static/js/2.1a261898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9abaef34b20df012df",
+    "url": "/MyPythonProject/static/js/main.f4e86239.chunk.js"
+  },
+  {
+    "revision": "fc37c8662b2f5b697c91",
+    "url": "/MyPythonProject/static/js/runtime-main.98b4d71f.js"
+  },
+  {
+    "revision": "bdb618866e11a2dbdc369318ff1ed95e",
+    "url": "/MyPythonProject/static/media/cat_cry.bdb61886.jpg"
+  },
+  {
+    "revision": "affb7e999820738c7ec0795cc292433a",
+    "url": "/MyPythonProject/static/media/cat_like.affb7e99.jpg"
+  },
+  {
+    "revision": "bfca78228c8003c9769edf4e5e97a862",
+    "url": "/MyPythonProject/static/media/cat_notfound.bfca7822.jpg"
+  },
+  {
+    "revision": "0634fc1378e12761480b7669682a0977",
+    "url": "/MyPythonProject/static/media/paper.0634fc13.png"
+  },
+  {
+    "revision": "d9ab5b5e85e12c34d76fc3bf351354b7",
+    "url": "/MyPythonProject/static/media/scissors.d9ab5b5e.png"
+  },
+  {
+    "revision": "82874705930a753c3f63c8ff3ec55199",
+    "url": "/MyPythonProject/static/media/stone.82874705.png"
+  },
+  {
+    "revision": "081232d0cbe4b2e3402e6e53634c36e3",
+    "url": "/MyPythonProject/static/media/vs.081232d0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a487540efb527c0be2f0e80322f3ccc2.js"
+  "/MyPythonProject/precache-manifest.5963031cd4acafc1088a7cbccd989d1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyPythonProject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
